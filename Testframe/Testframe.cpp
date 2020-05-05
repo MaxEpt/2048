657710141889c7cf8f583bb52c735b7c384808b8
MaxEpt <mak_sim93@mail.ru>
2020-05-06 02:16:56 +0300

    Tests for shift methods

--- Testframe/Testframe.cpp
+++ Testframe/Testframe.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "..\2048\GameBoard.h"
+#include "shiftBoardUP_Ags.h"
+#include "shiftBoardDown_Args.h"
+#include "shiftBoardLeft_Args.h"
+#include "shiftBoardRight_Args.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,34 +16,74 @@ namespace Testframe
 		
 		TEST_METHOD(ShiftBoardLeft)
 		{
+			GameBoard gb;
+			wchar_t message[12];
+
+			for (int i = 0; i < boardLeft_STEPS_COUNT; i++)
+			{
+				for (int j = 0; j < 4; j++)
+					for (int k = 0; k < 4; k++)
+						gb.placeValue(k, j, boardLeft_initialGbStates[i][j][k]);
+
+				gb.shiftBoardLeft();
+
+				swprintf(message, 20, L"step = %d", i);
+				Assert::AreEqual<int>(0, memcmp(gb.getGameBoard(), boardLeft_expectedGbStates[i], sizeof(boardLeft_expectedGbStates[i])), message);
+			}
 		}
 		
 		TEST_METHOD(ShiftBoardRight)
 		{
+			GameBoard gb;
+			wchar_t message[12];
+
+			for (int i = 0; i < boardRight_STEPS_COUNT; i++)
+			{
+				for (int j = 0; j < 4; j++)
+					for (int k = 0; k < 4; k++)
+						gb.placeValue(k, j, boardRight_initialGbStates[i][j][k]);
+
+				gb.shiftBoardRight();
+
+				swprintf(message, 20, L"step = %d", i);
+				Assert::AreEqual<int>(0, memcmp(gb.getGameBoard(), boardRight_expectedGbStates[i], sizeof(boardRight_expectedGbStates[i])), message);
+			}
 		}
 		
 		TEST_METHOD(ShiftBoardUp)
 		{
 			GameBoard gb;
+			wchar_t message[12];
 
-			gb.placeValue(0, 0, 2);
-			gb.placeValue(0, 3, 2);
-
-			gb.shiftBoardUp();
-			
-			unsigned int aExpectedBoard[4][4] =
+			for (int i = 0; i < boardUp_STEPS_COUNT; i++)
 			{
-				{4,0,0,0},
-				{0,0,0,0},
-				{0,0,0,0},
-				{0,0,0,0},
-			};
+				for (int j = 0; j < 4; j++)
+					for (int k = 0; k < 4; k++)
+						gb.placeValue(k, j, boardUp_initialGbStates[i][j][k]);				
+				
+				gb.shiftBoardUp();
 
-			Assert::AreEqual<int>(0, memcmp(gb.getGameBoard(), aExpectedBoard, 16));
+				swprintf(message, 20, L"step = %d", i);
+				Assert::AreEqual<int>(0, memcmp(gb.getGameBoard(), boardUp_expectedGbStates[i], sizeof(boardUp_expectedGbStates[i])), message);
+			}
 		}
 		
 		TEST_METHOD(ShiftBoardDown)
 		{
+			GameBoard gb;
+			wchar_t message[12];
+
+			for (int i = 0; i < boardDown_STEPS_COUNT; i++)
+			{
+				for (int j = 0; j < 4; j++)
+					for (int k = 0; k < 4; k++)
+						gb.placeValue(k, j, boardDown_initialGbStates[i][j][k]);
+				
+				gb.shiftBoardDown();
+
+				swprintf(message, 20, L"step = %d", i);
+				Assert::AreEqual<int>(0, memcmp(gb.getGameBoard(), boardDown_expectedGbStates[i], sizeof(boardDown_expectedGbStates[i])), message);
+			}
 		}
 	};
 }
